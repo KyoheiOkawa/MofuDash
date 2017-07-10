9792cd062d34dad996d2b7450a7349029f9d92db
Kyohei <kurokenken0310@gmail.com>
2017-07-10 23:02:49 +0900

    暫定的にタイトルシーンを作成

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour {
+    [SerializeField]
+    string m_TestNextScene = "PlayerTest";//テスト用のシーン遷移先
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene(m_TestNextScene);
+    }
+}
