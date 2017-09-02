557e427a9d9b2b34f3be298f09f0b9a943bd4938
Kyohei <kurokenken0310@gmail.com>
2017-09-02 21:37:03 +0900

    オブジェクトに埋まっているときに色変更しようとしたときに出るエフェクトを作成

---
+++ Assets/Scripts/Batsu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batsu : MonoBehaviour
+{
+    [SerializeField]
+    float destroyTime = 0.5f;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, destroyTime);
+    }
+}
